43887159484eb8b85ed9aeee9bd90dc6665eafa4
Lucy <lucymidgley13@gmail.com>
2020-01-20 22:50:30 +0000

    Added a template for the index page and added a table for the urls and their shortened forms and a (dead) link to create a new short url

--- express_server.js
+++ express_server.js
@@ -6,7 +6,8 @@ app.set("view engine", "ejs");
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  "9sm5xK": "http://www.google.com",
+  "example" : "http://example.com/"
 };
 
 app.get("/", (req, res) => {
@@ -24,3 +25,8 @@ app.get("/urls.json", (req, res) => {
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n")
 })
+
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
