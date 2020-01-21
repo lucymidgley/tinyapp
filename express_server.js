94112a1aa7917e84dd0ad3a0d2fad9edd976f592
Lucy <lucymidgley13@gmail.com>
2020-01-21 00:38:58 +0000

    Added a function to generate random 6 digit alphanumeric strings and a form for the user to post their URL to and a path to this new page and a basic response for this post request.

--- express_server.js
+++ express_server.js
@@ -2,6 +2,15 @@ const express = require("express");
 const app = express();
 const PORT = 8080;
 
+function generateRandomString() {
+  const arr = [1,2,3,4,5,6,7,8,9,0,"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  let ans = ""
+  for(let i=0; i<6; i++){
+    ans += arr[Math.floor(Math.random() * arr.length)];
+  }
+  return ans
+  }
+
 app.set("view engine", "ejs");
 
 const urlDatabase = {
@@ -9,6 +18,9 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com",
   "example" : "http://example.com/"
 };
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -31,7 +43,16 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
